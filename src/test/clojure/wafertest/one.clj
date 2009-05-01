35e25a3e700494be300d0b966694fb703288da54
Howard M. Lewis Ship <hlship@gmail.com>
2009-05-01 10:48:58 -0700

    Resolve dependencies using Maven.

--- src/test/clojure/wafertest/one.clj
+++ src/test/clojure/wafertest/one.clj
@@ -1,10 +1,13 @@
 (ns wafertest.one
-    (:use com.howardlewisship.wafer com.howardlewisship.wafer.parser))
+    (:use com.howardlewisship.wafer com.howardlewisship.wafer.parser clojure.xml))
+
+(set! *warn-on-reflection* true)
 
 (def t (parse-view-fragment-template "src/test/resources/wafertest/one/views/simple.xml"))
 
 (println "parsed function: " t)
 (println "generated DOM:   " (t nil))
 (println "Markup:\n" (dom-to-string (t nil)))
-         
 
+
+(println "ns.xml: " (parse "src/test/resources/wafertest/one/views/ns.xml"))
