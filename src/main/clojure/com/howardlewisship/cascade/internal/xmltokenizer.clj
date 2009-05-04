b90e7186edfcfe373aa4df75c27d82340907929d
Howard M. Lewis Ship <hlship@gmail.com>
2009-05-04 14:25:39 -0700

    Add namespace support to xmltokenizer.

--- src/main/clojure/com/howardlewisship/cascade/internal/xmltokenizer.clj
+++ src/main/clojure/com/howardlewisship/cascade/internal/xmltokenizer.clj
@@ -9,12 +9,12 @@
 
 
 (defstruct xml-token
-  :type :location ; used for all
-  :ns-uri ; used for :start-element, :begin-ns-prefix and :attribute
-  :ns-prefix ; used for :begin-ns-prefix and :end-ns-prefix
-  :tag ; used for :start-element
-  :name ; used for :attribute
-  :value ; used for :text and :comment
+  :type :location ; keyword: used for all
+  :ns-uri ; string: used for :start-element, :begin-ns-prefix and :attribute
+  :ns-prefix ; keyword: used for :begin-ns-prefix and :end-ns-prefix
+  :tag ; keyword: used for :start-element
+  :name ; keyword: used for :attribute
+  :value ; string: used for :text and :comment
   )
 
 (defstruct location :resource :line)
@@ -38,10 +38,12 @@
 ; doesn't fit well into the functional world, so we have to have some thread-local mutable state.
 
 (def *tokens*)
-(def *buffer*)
+(def #^StringBuilder *buffer*)
 (def *resource*)
 (def *text-location*)
-(def *locator*)
+(def #^Locator *locator*)
+(def *line*)
+(def *location*)
 
 (defn- to-keyword
   "Converts a string into a keyword."
@@ -56,17 +58,20 @@
 (defn- current-location
   "Current location."
   []
-  ; TODO: this is invoked far more often than the line number changes,
-  ; so do some caching.
-  (struct location *resource* (.getLineNumber *locator*)))
+  ; A little optimization here, to reuse *location* if the line number has not changed since the
+  ; last call.
+  (let [line (.getLineNumber *locator*)]
+       (when (or (nil? *location*) (not= (*location* :line) line))
+             (set! *location* (struct location *resource* line))))
+  *location*)
 
 (defn- add-attribute-tokens
   "Adds a token for each attribute."
   [#^Attributes attrs]
-  (doseq [x (range (. attrs getLength))]
-         (let [uri (. attrs (getURI x))
-               name (. attrs (getLocalName x))
-               value (. attrs (getValue x))
+  (doseq [#^Integer x (range (.getLength attrs))]
+         (let [uri (.getURI attrs x)
+               name (.getLocalName attrs x)
+               value (.getValue attrs x)
                token (struct-map xml-token
                                  :type :attribute
                                  :location (current-location)
@@ -80,12 +85,15 @@
   "Adds a text token if there's any text."
   []
   (when (pos? (.length *buffer*))
-        (add-token (struct-map xml-token :type :text :value (.toString *buffer*) :location *text-location*))
+        (add-token (struct-map xml-token
+                               :type :text
+                               :location *text-location*
+                               :value (.toString *buffer*)))
         (set! *text-location* nil)
         (.setLength *buffer* 0)))
 
 (defn- add-text
-  "Adds text to the buffer and manages the *text-location var."
+  "Adds text to the buffer and manages the *text-location* var."
   [ch start length]
   (.append *buffer* ch start length)
   (when (nil? *text-location*)
@@ -104,13 +112,27 @@
 
          (endElement [uri local-name q-name]
                      (flush-text)
-                     (add-token (struct-map xml-token :type :end-element :location (current-location))))
+                     (add-token (struct-map xml-token
+                                            :type :end-element
+                                            :location (current-location))))
 
          (ignorableWhitespace [ch start length]
                               (add-text ch start length))
 
          (characters [ch start length]
                      (add-text ch start length))
+
+         (startPrefixMapping [prefix uri]
+                             (add-token (struct-map xml-token
+                                                    :type :begin-ns-prefix
+                                                    :location (current-location)
+                                                    :ns-prefix (to-keyword prefix)
+                                                    :ns-uri uri)))
+         (endPrefixMapping [prefix]
+                           (add-token (struct-map xml-token
+                                                  :type :end-ns-prefix
+                                                  :location (current-location)
+                                                  :ns-prefix (to-keyword prefix))))
 
          ; This gets invoked once, early. The provided Locator is mutable.
 
@@ -125,7 +147,9 @@
             *buffer* (StringBuilder.)
             *resource* src
             *locator* nil
-            *text-location* nil]
+            *text-location* nil
+            *line* nil
+            *location* nil]
            (let [factory (SAXParserFactory/newInstance)]
                 (.setNamespaceAware factory true)
                 (.. factory newSAXParser (parse src sax-handler))
--- src/test/clojure/com/howardlewisship/cascade/internal/test-xml-tokenizer.clj
+++ src/test/clojure/com/howardlewisship/cascade/internal/test-xml-tokenizer.clj
@@ -1,13 +1,18 @@
+(set! *warn-on-reflection* true)
+
 (ns com.howardlewisship.cascade.internal.test-xml-tokenizer
     (:use clojure.contrib.test-is com.howardlewisship.cascade.internal.xmltokenizer))
-
-(set! *warn-on-reflection* true)
 
 (def base "src/test/resources/com/howardlewisship/cascade/internal/")
 
 (defn expect
   [f tokens expected]
   (is (= (map f tokens) expected)))
+
+(defn by-type
+  "Matches tokens by type"
+  [type tokens]
+  (filter #(= (% :type) type) tokens))
 
 (deftest parse-without-namespace
          (let [tokens (tokenize-xml (str base "simple.xml"))]
@@ -22,15 +27,32 @@
 
               (expect #(-> % :location :line) tokens [2 2 4 4 4 6 6])
 
-              (expect :tag (filter #(= (% :type) :start-element) tokens) [:root :flintstone])
+              (expect :tag (by-type :start-element tokens) [:root :flintstone])
 
               (let [attr (tokens 1)]
                    (is (= (attr :name) :id))
                    (is (= (attr :value) "fred")))
 
-              (is (= (.trim #^String ((tokens 2) :value)) "Before"))
+              (is (= (.trim #^String ((tokens 2) :value)) "Before"))))
+
+
+(deftest parse-with-namespaces
+         (let [tokens (tokenize-xml (str base "ns.xml"))]
+
+              (expect :type tokens [:begin-ns-prefix :start-element :attribute :attribute :text :start-element
+                                    :attribute :end-element :begin-ns-prefix :text :start-element :end-element
+                                    :end-ns-prefix :text :end-element :end-ns-prefix])
+
+              (expect :ns-uri (by-type :begin-ns-prefix tokens) ["clojure" "zeta/1.0"])
+
+              (expect :ns-prefix (by-type :begin-ns-prefix tokens) [:c :z])
+
+              (expect :ns-prefix (by-type :end-ns-prefix tokens) [:z :c])
+
+              (expect :ns-uri (by-type :start-element tokens) ["clojure" "" "zeta/1.0"])
+
+              (expect :ns-uri (by-type :attribute tokens) ["" "clojure" ""])
 
               ))
 
-(time (run-tests))
-
+(run-tests)
