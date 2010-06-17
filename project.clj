d2a67d4668b62332a7e63300db1875cdbe8cba49
Howard M. Lewis Ship <hlship@gmail.com>
2010-06-17 13:50:39 -0700

    Update dependencies to pull Selenium with "standalone" classifier, identify test resources path

--- project.clj
+++ project.clj
@@ -12,13 +12,11 @@
     [[org.eclipse.jetty/jetty-server "7.0.0.RC4"]
      [org.eclipse.jetty/jetty-servlet "7.0.0.RC4"]     
      [org.easymock/easymock "2.5.1"]
-     [org.seleniumhq.selenium.client-drivers/selenium-java-client-driver "1.0.3"]
-     ; oops, need the right classification of this, not yet supported
-     [org.seleniumhq.selenium.server/selenium-server "1.0.3"] 
-     [org.seleniumhq.selenium.server/selenium-server-coreless "1.0.3"]]
+     [org.seleniumhq.selenium.server/selenium-server "1.0.3" :classifier "standalone"]]
   :aot :all
   :warn-on-reflection true
   :source-path "src/main/clojure"
   :test-path "src/test/clojure"
   :resources-path "src/main/resources"
+  :test-resources-path "src/test/resources"
   :jar-dir "target")
