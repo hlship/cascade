53e310da3e10d3ff413d6cb88249f2e01c977ff6
Howard M. Lewis Ship <hlship@gmail.com>
2011-10-27 10:17:29 -0700

    Start putting in place support for assets

--- src/main/clojure/cascade.clj
+++ src/main/clojure/cascade.clj
@@ -16,6 +16,7 @@
   cascade
   "Core functions and macros used when implementing Cascade views"
   (:use
+    [compojure core]
     [cascade dom]
     [cascade.internal viewbuilder parse-functions]))
 
@@ -57,6 +58,41 @@
   [comment]
   (comment-node comment))
 
+(def application-version
+  "The application version, which is incorporated into the URLs for assets." nil)
+
+; Should Asset extend Renderable?
+(defprotocol Asset
+  "Represent a server-side resource so that it can be exposed efficiently to the client."
+  (exists [asset] "Returns true if the asset exists and is readable.")
+  (content-type [asset] "Returns the MIME content type for the Asset.")
+  (^InputStream content [asset] "Returns the content of the Asset as a stream of bytes.")
+  (^String client-url [asset] "Returns an absolute URL to the Asset."))
+
+(defn file-asset-handler [req] nil)
+
+(defn wrap-exception-handling
+  "Middleware for standard Cascade exception reporting; exceptions are caught and reported using the Cascade
+  exception report view."
+  [handler]
+  ; Just a placeholder for now
+  handler)
+
+(defn initialize-assets
+  "Initializes asset handling for Cascade. This sets an application version (a value incorporated into URLs, which
+should change with each new deployment. Named arguments:
+:virtual-folder (default \"assets\")
+  The root folder under which assets will be exposed to the client.
+:public-folder (default \"public\")
+  The file system folder under which file assets are stored."
+  [application-version & {:keys [virtual-folder public-folder]
+                          :or {virtual-folder "asset"
+                               public-folder "public"}}]
+  (set! cascade/application-version application-version)
+  (let [root (str "/" virtual-folder "/" application-version)]
+    (wrap-exception-handling
+      (routes
+        (GET (str root "/file/*") file-asset-handler)))))
 
 (defn wrap-html
   "Ring middleware that wraps a handler so that the return value from the handler (a seq of DOM nodes)
