57e61d8d11bc0bdf07744dd3e1e4ff26f4d9c5d2
Howard M. Lewis Ship <hlship@gmail.com>
2011-11-10 15:36:32 -0800

    Setup test app to execute as "lein ring server"

--- project.clj
+++ project.clj
@@ -1,10 +1,13 @@
 (defproject cascade "0.2-SNAPSHOT"
   :description "Simple, fast, easy web applications in idiomatic Clojure"
   :url "http://github.com/hlship/cascade"
+  :main main
   :warn-on-reflection true
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/algo.monads "0.1.0"]
                  [compojure "0.6.5"]]
   :dev-dependencies [[ring/ring-jetty-adapter "0.3.11"]
                      [swank-clojure "1.3.1"]
-                     [midje "1.1.1"]])
+                     [midje "1.1.1"]
+                     [lein-ring "0.4.6"]]
+  :ring {:handler main/app})
