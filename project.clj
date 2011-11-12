d4eedea019456231ad764017558dd4b7b55ddb47
Howard M. Lewis Ship <hlship@gmail.com>
2011-11-12 11:25:05 -0800

    Remove the :main value from project.clj

--- project.clj
+++ project.clj
@@ -1,13 +1,13 @@
 (defproject cascade "0.2-SNAPSHOT"
   :description "Simple, fast, easy web applications in idiomatic Clojure"
   :url "http://github.com/hlship/cascade"
-  :main main
   :warn-on-reflection true
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/algo.monads "0.1.0"]
                  [clj-json "0.4.3"]
                  [compojure "0.6.5"]]
   :dev-dependencies [[ring/ring-jetty-adapter "0.3.11"]
+                     [hiccup "0.3.7"]
                      [swank-clojure "1.3.1"]
                      [midje "1.1.1"]
                      [lein-ring "0.4.6"]]
